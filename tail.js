2266b1a6878bd011aa27771796b69b97c49d8e3c
Muaad Ahmed <muaad.abdi.ahmed@gmail.com>
2021-10-26 00:20:11 +0000

    Added a tail function to lotide lib

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+const tail = function(array) {
+  let tail = [];
+  for (let i = 1; i < array.length; i++) {
+    tail.push(array[i]);
+  }
+  return tail;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], 'Labs');
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
